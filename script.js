59441caecaf42c46bec8ecfa99e31d63737eef55
Eduarda <eduarda.js.brandao@gmail.com>
2023-05-30 01:05:23 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMode(){
+    const html = document.documentElement;
+
+    html.classList.toggle('light');
+    //  verificando se a classe CSS especificada está presente ou não.
+    //  efeito de alternância dos modos light e dark.
+
+}
